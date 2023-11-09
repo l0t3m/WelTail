c07b461235f182597d0f0b1963b5b5f5748ce1e9
lotem <lotem1shriker@gmail.com>
2023-11-09 17:46:16 +0200

    Improved code

--- functions.py
+++ functions.py
@@ -6,61 +6,10 @@ import db, datetime, time
 
 #################### Main Functions: ####################
 
-def addActivity(user_id, pet_id, type, name, nextAlert:str, repeat, repeatType, repeatAmount):
-    repeat = "1" if repeat == "on" else "0"
-
-    if repeatType == "hours":
-        repeatInterval = int(repeatAmount) * 3600
-    elif repeatType == "days":
-        repeatInterval = int(repeatAmount) * 86400
-    elif repeatType == "weeks":
-        repeatInterval = int(repeatAmount) * 604800
-    else:
-        repeatInterval = int(repeatAmount) * 2629743
-    
-    year, month, day = nextAlert[:-6].split("-")
-    hour, minute = nextAlert[-5:].split(":")
-    nextAlert = int((datetime.datetime(int(year), int(month), int(day), int(hour), int(minute))).timestamp())
-    db.query(f"INSERT INTO activities (user_id, pet_id, type, name, repeat, nextAlert, repeatInterval) VALUES ({user_id}, {pet_id}, '{type}', '{name}', {repeat}, '{nextAlert}', '{repeatInterval}');")
-
-
-
-#################### General Functions: ####################
-
 def getUserData(username):
     for user in db.get_TableDicts("SELECT * FROM users"):
         if user['username'].lower() == username.lower():
             return user
-
-
-def updateUserAlerts(user_id):
-    '''Checks if the user activities are up to date, if not, fixes them. \n
-    Returns the amount of activities fixed.'''
-    updatedCounter = 0
-
-    for activity in db.get_TableDicts(f"SELECT * FROM activities WHERE user_id = '{user_id}'"):
-        if int(activity['nextAlert']) < int(time.time()):
-            if int(activity['repeat']) == 0:
-                db.query(f"DELETE FROM activities WHERE activity_id = {activity['activity_id']}")
-
-            next = generate_nextAlert(int(activity['nextAlert']), int(activity['repeatInterval']), int(datetime.datetime.now().timestamp()))
-            db.query(f"UPDATE activities SET nextAlert = '{next}' WHERE activity_id = '{activity['activity_id']}'")
-            updatedCounter += 1
-            
-    return updatedCounter
-
-
-def getUpcomingAlerts(user_id):
-    '''Returns all the alerts for today.'''
-    updateUserAlerts(user_id)
-    alerts = []
-
-    for activity in db.get_TableDicts(f"SELECT * FROM activities WHERE user_id = '{user_id}' ORDER BY nextAlert"):
-        next = generateCountdown(int(activity['nextAlert']))
-        if next <= generate_timeUntilEndOfDay():
-            alerts.append(activity)
-    
-    return alerts
 
 
 def getPetActivities(user_id, pet_id):
@@ -84,6 +33,67 @@ def getAllActivities(user_id):
     return activities
 
 
+def getUpcomingAlerts(user_id):
+    '''Returns all the alerts for today.'''
+    updateUserAlerts(user_id)
+    alerts = []
+
+    for activity in db.get_TableDicts(f"SELECT * FROM activities WHERE user_id = '{user_id}' ORDER BY nextAlert"):
+        next = generateCountdown(int(activity['nextAlert']))
+        if next <= generate_timeUntilEndOfDay():
+            alerts.append(activity)
+    
+    return alerts
+
+
+
+#################### General Functions: ####################
+
+def updateUserAlerts(user_id):
+    '''Checks if the user activities are up to date, if not, fixes them. \n
+    Returns the amount of activities fixed.'''
+    updatedCounter = 0
+
+    for activity in db.get_TableDicts(f"SELECT * FROM activities WHERE user_id = '{user_id}'"):
+        if int(activity['nextAlert']) < int(time.time()):
+            if int(activity['repeat']) == 0:
+                db.query(f"DELETE FROM activities WHERE activity_id = {activity['activity_id']}")
+
+            next = generate_nextAlert(int(activity['nextAlert']), int(activity['repeatInterval']), int(datetime.datetime.now().timestamp()))
+            db.query(f"UPDATE activities SET nextAlert = '{next}' WHERE activity_id = '{activity['activity_id']}'")
+            updatedCounter += 1
+            
+    return updatedCounter
+
+
+def addActivity(user_id, pet_id, type, name, nextAlert:str, repeat:str, repeatType:str, repeatAmount):
+    repeat = "1" if repeat == "on" else "0"
+
+    if repeatType == "hours":
+        repeatInterval = int(repeatAmount) * 3600
+    elif repeatType == "days":
+        repeatInterval = int(repeatAmount) * 86400
+    elif repeatType == "weeks":
+        repeatInterval = int(repeatAmount) * 604800
+    else:
+        repeatInterval = int(repeatAmount) * 2629743
+    
+    year, month, day = nextAlert[:-6].split("-")
+    hour, minute = nextAlert[-5:].split(":")
+    nextAlert = int((datetime.datetime(int(year), int(month), int(day), int(hour), int(minute))).timestamp())
+    db.query(f"INSERT INTO activities (user_id, pet_id, type, name, repeat, nextAlert, repeatInterval) VALUES ({user_id}, {pet_id}, '{type}', '{name}', {repeat}, '{nextAlert}', '{repeatInterval}');")
+
+
+def activity_done(activity_id):
+    activity = db.get_TableDicts(f"SELECT * FROM activities WHERE activity_id = '{activity_id}';")
+    
+    if activity[0]['repeat'] == 0:
+        db.query(f"DELETE FROM activities WHERE activity_id = '{activity_id}';")
+    else:
+        next = generate_nextAlert(int(activity[0]['nextAlert']), int(activity[0]['repeatInterval']), int(activity[0]['nextAlert']))
+        db.query(f"UPDATE activities SET nextAlert = '{next}' WHERE activity_id = '{activity[0]['activity_id']}'")
+
+
 def reformat_Activities(activities:list):
     '''Gets an activity list, containing dict. Returns the dicts reformatted.'''
     newList = []
@@ -104,16 +114,6 @@ def reformat_Activities(activities:list):
         }
         newList.append(newDict)
     return newList
-
-
-def activity_done(activity_id):
-    activity = db.get_TableDicts(f"SELECT * FROM activities WHERE activity_id = '{activity_id}';")
-    
-    if activity[0]['repeat'] == 0:
-        db.query(f"DELETE FROM activities WHERE activity_id = '{activity_id}';")
-    else:
-        next = generate_nextAlert(int(activity[0]['nextAlert']), int(activity[0]['repeatInterval']), int(activity[0]['nextAlert']))
-        db.query(f"UPDATE activities SET nextAlert = '{next}' WHERE activity_id = '{activity[0]['activity_id']}'")
 
 
 def generate_greetingMessage():
