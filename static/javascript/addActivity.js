d9d279a4cbf88e3fc880d4b516c690521cc925f9
lotem <lotem1shriker@gmail.com>
2023-11-09 17:51:54 +0200

    Improved code

--- static/javascript/addActivity.js
+++ static/javascript/addActivity.js
@@ -1,15 +1,12 @@
 function ShowPage(props) {
-    const [choice, setChoice] = React.useState("days");
-
     return (
         <div>
             <div className="field flex">
                 <div>Repeat this activity?</div>
 
-                <input type="radio" name="repeat" id="off" value="off" onChange={() => display.className = "hidden"}/>
+                <input type="radio" name="repeat" value="off" onChange={() => display.className = "hidden"}/>
                 <div>Off</div>
-
-                <input type="radio" name="repeat" id="on" value="on" onChange={() => display.className = "shown"}/>
+                <input type="radio" name="repeat" value="on" onChange={() => display.className = "shown"}/>
                 <div>On</div>
             </div>
 
@@ -19,7 +16,7 @@ function ShowPage(props) {
 
                     <input type="number" name="repeatAmount" min="1" placeholder="Number"/>
 
-                    <select name="repeatType" id="repeatType" onChange={() => setChoice(repeatType.selectedOptions[0].value)}>
+                    <select name="repeatType" id="repeatType">
                         <option value="hours">Hour/s</option>
                         <option value="days">Day/s</option>
                         <option value="weeks">Week/s</option>
